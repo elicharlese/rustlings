ef60f2d19f4650a5693466d4a577a3104e6dc84b
elicharlese <elicharles.e@gmail.com>
2023-02-21 04:12:11 +0000

    adding notes

--- personal/projects/guessing_game/src/main.rs
+++ personal/projects/guessing_game/src/main.rs
@@ -15,7 +15,9 @@ fn main() { // `fn` declares a new function, `main` is the main function
     io::stdin() // we could use std::io::stdin() to read from the standard input
     // std::io:Stdin is a type that implements the Read trait
         .read_line(&mut guess)
+        // Hence, you need to write &mut guess rather than &guess to make it mutable.
         .expect("Failed to read line");
+        // io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {guess}");
     // println!("You guessed: {}", guess);
 }
@@ -23,6 +25,13 @@ fn main() { // `fn` declares a new function, `main` is the main function
 // An associated function is a function that’s implemented on a type, in this case String.
 // This new function creates a new, empty string.
 // You’ll find a new function on many types, because it’s a common name for a function that makes a new value of some kind.
+// References are a complex feature, and one of Rust’s major advantages is how safe and easy it is to use references.
+
+// Result is an enumeration, often called an enum, which is a type that can be in one of multiple possible states. 
+// We call each possible state a variant.
+// The purpose of these Result types is to encode error-handling information.
+
+
 
 
 
