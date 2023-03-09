f652033583a8736e4c27664c576baf3f9f692d67
elicharlese <elicharles.e@gmail.com>
2023-03-09 23:52:52 +0000

    Adding source code

--- personal/projects/guessing_game/src/main.rs
+++ personal/projects/guessing_game/src/main.rs
@@ -81,19 +81,31 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() { // Ignoring a non-number guess and asking for another guess instead of crashing the program
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
 
-    let guess: u32 = guess.trim().parse().expect("Invalid input!");
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            // A match expression is made up of arms. An arm consists of a pattern to match
+            // against, and the code that should be run if the value given to match fits that arm’s pattern
+            // The program will now ask for another guess forever, which actually introduces a new problem.
+            // It doesn’t seem like the user can quit!
+            // The user could always interrupt the program by using the keyboard shortcut ctrl-c.
+        }
     }
 }
